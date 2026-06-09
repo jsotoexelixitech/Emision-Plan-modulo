ca34738272b510dc913da2b67fd1ad5dcb6c9e2e
Exelixi Tech <jsotoexelixitech@github.com>
2026-06-09 09:57:06 -0400

    feat: add EmisionConfigPanel premium UI and mapping

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,10 +5,18 @@ import App from './App.tsx'
 import './lib/bridge'
 import { NexusGuard } from './nexus/NexusGuard'
 
+import { EmisionConfigPanel } from './config/EmisionConfigPanel'
+
+const isConfigRoute = window.location.pathname === '/config';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <NexusGuard recheckInterval={30}>
-      <App />
-    </NexusGuard>
+    {isConfigRoute ? (
+      <EmisionConfigPanel />
+    ) : (
+      <NexusGuard recheckInterval={30}>
+        <App />
+      </NexusGuard>
+    )}
   </StrictMode>,
 )
